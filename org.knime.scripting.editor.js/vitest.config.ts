1ecb2701eacc3b6692253c86ad60e42330a25fe9
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2023-08-01 15:49:51 +0200

    AP-20767: Add coverage config
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [645008daa078399760cddc62b393f08452c3eb0b](https://github.com/knime/knime-scripting-editor/commit/645008daa078399760cddc62b393f08452c3eb0b)

--- org.knime.scripting.editor.js/vitest.config.ts
+++ org.knime.scripting.editor.js/vitest.config.ts
@@ -1,14 +1,14 @@
 import { fileURLToPath } from "node:url";
 import { mergeConfig, defineConfig } from "vite";
-import { configDefaults } from "vitest/config";
 import viteConfig from "./vite.config";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      include: ["src/**/__tests__/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+      exclude: ["**/node_modules/**", "**/dist/**", "webapps-common/**"],
       environment: "jsdom",
-      exclude: [...configDefaults.exclude, "e2e/*"],
       root: fileURLToPath(new URL("./", import.meta.url)),
       transformMode: {
         web: [/\.[jt]sx$/],
@@ -16,6 +16,21 @@ export default mergeConfig(
       setupFiles: [
         fileURLToPath(new URL("test-setup/vitest.setup.js", import.meta.url)),
       ],
+      coverage: {
+        all: true,
+        exclude: [
+          "coverage/**",
+          "dist/**",
+          "webapps-common/**",
+          "lib/**",
+          "**/*.d.ts",
+          "**/__tests__/**",
+          "test-setup/**",
+          "**/{vite,vitest,postcss}.config.{js,cjs,mjs,ts}",
+          "**/.{eslint,prettier,stylelint}rc.{js,cjs,yml}",
+        ],
+        reporter: ["html", "text", "lcov"],
+      },
     },
   }),
 );
