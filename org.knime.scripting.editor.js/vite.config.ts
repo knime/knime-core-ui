52d5a3b005bd3c1a8d1d19405e61fdb90906217e
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2023-08-02 14:48:47 +0200

    AP-20767: Add build step
    (This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: 27369d3079c0d91b43de3dc57f1875d529789367)

--- org.knime.scripting.editor.js/vite.config.ts
+++ org.knime.scripting.editor.js/vite.config.ts
@@ -3,13 +3,30 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL("lib/main.ts", import.meta.url)),
+      fileName: "knime-scripting-editor",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["vue"],
     },
   },
 });
