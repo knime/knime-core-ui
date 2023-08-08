5d7f9d781ea6912ffe678bdbd053d9896719ac86
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2023-08-08 17:06:17 +0200

    AP-20821: Add scripting service singleton
    (This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: d24b44c2abad874f67e76648f25a7fef0ba87e84)

---
+++ org.knime.scripting.editor.js/src/__tests__/scripting-service.test.ts
@@ -0,0 +1,236 @@
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import sleep from "webapps-common/util/sleep";
+
+import {
+  DialogService,
+  IFrameKnimeService,
+  JsonDataService,
+} from "@knime/ui-extension-service";
+import type { ScriptingServiceType } from "../scripting-service";
+
+vi.mock("@knime/ui-extension-service", () => ({
+  IFrameKnimeService: vi.fn(),
+  JsonDataService: vi.fn(),
+  DialogService: vi.fn(),
+}));
+
+const lock = () => {
+  let resolve: () => void = () => {};
+  const promise = new Promise<void>((r) => {
+    resolve = r;
+  });
+  return { promise, resolve };
+};
+
+describe("scripting-service", () => {
+  let _knimeService: any,
+    _jsonDataService: any,
+    _dialogService: any,
+    getScriptingService: (mock?: ScriptingServiceType) => ScriptingServiceType;
+
+  beforeEach(async () => {
+    // Make sure the module is reloaded to reset the singleton instance
+    vi.resetModules();
+    getScriptingService = (await import("../scripting-service"))
+      .getScriptingService;
+
+    // Mock the services
+    _knimeService = {
+      waitForInitialization: vi.fn(() => Promise.resolve()),
+    };
+    vi.mocked(IFrameKnimeService).mockReturnValue(_knimeService);
+
+    _jsonDataService = {
+      registerDataGetter: vi.fn(() => {}),
+      initialData: vi.fn(() => ({ script: "foo" })),
+      data: vi.fn(() => {}),
+      applyData: vi.fn(() => {}),
+    };
+    vi.mocked(JsonDataService).mockReturnValue(_jsonDataService);
+
+    _dialogService = {
+      getFlowVariableSettings: vi.fn(() => Promise.resolve({})),
+    };
+    vi.mocked(DialogService).mockReturnValue(_dialogService);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("waits for knime service initialization", async () => {
+    const sendToServiceResolved = vi.fn();
+    const sendToServiceRejected = vi.fn();
+
+    // Replace the mock such that we can block the initialization
+    const { promise, resolve } = lock();
+    _knimeService.waitForInitialization.mockReturnValue(promise);
+
+    const sendToServicePromise = getScriptingService()
+      .sendToService("dummy")
+      .then(() => {
+        sendToServiceResolved();
+      })
+      .catch(() => {
+        sendToServiceRejected();
+      });
+    await sleep(20);
+    expect(sendToServiceResolved).not.toHaveBeenCalled();
+    expect(sendToServiceRejected).not.toHaveBeenCalled();
+
+    // Now the initialization should be resolved
+    resolve();
+
+    await sendToServicePromise;
+    expect(sendToServiceResolved).toHaveBeenCalled();
+    expect(sendToServiceRejected).not.toHaveBeenCalled();
+  });
+
+  it("initializes the JsonDataService with the KnimeService", async () => {
+    await getScriptingService().sendToService("dummy");
+    expect(JsonDataService).toHaveBeenCalled();
+    expect(JsonDataService).toHaveBeenCalledWith(_knimeService);
+  });
+
+  it("initializes the DialogService with the KnimeService", async () => {
+    await getScriptingService().getFlowVariableSettings();
+    expect(DialogService).toHaveBeenCalled();
+    expect(DialogService).toHaveBeenCalledWith(_knimeService);
+  });
+
+  it("sends requests to the JsonDataService", async () => {
+    await getScriptingService().sendToService("dummy", ["foo", "bar"]);
+    expect(_jsonDataService.data).toHaveBeenCalledWith({
+      method: "dummy",
+      options: ["foo", "bar"],
+    });
+  });
+
+  it("uses the mock if provided", () => {
+    const mockScriptingService = vi.fn();
+
+    // @ts-ignore
+    const scriptingService = getScriptingService(mockScriptingService);
+    expect(scriptingService).toBe(mockScriptingService);
+
+    // All future calls should return the mock
+    expect(getScriptingService()).toBe(mockScriptingService);
+  });
+
+  describe("settings", () => {
+    it("gets initial data from the JsonDataService", async () => {
+      const initalSettings = await getScriptingService().getInitialSettings();
+      expect(initalSettings).toEqual({ script: "foo" });
+      expect(_jsonDataService.initialData).toHaveBeenCalledOnce();
+      expect(_jsonDataService.initialData).toHaveBeenCalledWith();
+    });
+
+    it("saves settings by calling JsonDataService.applyData", async () => {
+      await getScriptingService().saveSettings({ script: "bar" });
+      expect(_jsonDataService.applyData).toHaveBeenCalledOnce();
+    });
+
+    it("registers the data getter to return the last saved settings", async () => {
+      let dataGetter: (() => any) | null = null;
+      _jsonDataService.registerDataGetter.mockImplementation(
+        (providedDataGetter: () => any) => {
+          dataGetter = providedDataGetter;
+        },
+      );
+
+      const scriptingService = getScriptingService();
+      await scriptingService.saveSettings({ script: "bar" });
+      expect(dataGetter).not.toBeNull();
+      expect(dataGetter!()).toEqual({ script: "bar" });
+
+      await scriptingService.saveSettings({ script: "print('Hello world')" });
+      expect(dataGetter!()).toEqual({ script: "print('Hello world')" });
+    });
+  });
+
+  describe("events handler", () => {
+    it("polls events from the service", async () => {
+      // Scripting service initialization starts the event poller
+      getScriptingService();
+
+      // Calling the data resolves the promise
+      // We can check afterwards if the arguments were right
+      const { promise, resolve } = lock();
+      _jsonDataService.data.mockImplementation(resolve);
+
+      await promise;
+      expect(_jsonDataService.data).toHaveBeenCalledWith({
+        method: "getEvent",
+      });
+    });
+
+    it("calls the correct event handler", async () => {
+      const scriptingService = getScriptingService();
+
+      // Event handler for foo (the first event resolves the promise)
+      const { promise: eventFooPromise, resolve: resolveFooEvent } = lock();
+      const eventHandlerFoo = vi.fn(resolveFooEvent);
+      scriptingService.registerEventHandler("foo", eventHandlerFoo);
+
+      // Event handler for bar (the first event resolves the promise)
+      const { promise: eventBarPromise, resolve: resolveBarEvent } = lock();
+      const eventHandlerBar = vi.fn(resolveBarEvent);
+      scriptingService.registerEventHandler("bar", eventHandlerBar);
+
+      // Let the next data call return events for foo and bar
+      _jsonDataService.data.mockImplementationOnce(() => {
+        return Promise.resolve({ type: "bar", data: "data for bar" });
+      });
+      _jsonDataService.data.mockImplementationOnce(() => {
+        return Promise.resolve({ type: "foo", data: "data for foo" });
+      });
+
+      await eventFooPromise;
+      expect(eventHandlerFoo).toHaveBeenCalledWith("data for foo");
+
+      await eventBarPromise;
+      expect(eventHandlerBar).toHaveBeenCalledWith("data for bar");
+    });
+
+    it("calls the language-server event handler", async () => {
+      const scriptingService = getScriptingService();
+
+      const { promise, resolve } = lock();
+      const eventHandler = vi.fn(resolve);
+      scriptingService.registerLanguageServerEventHandler(eventHandler);
+
+      // Let the next data call return an event for the language server
+      _jsonDataService.data.mockImplementationOnce(() => {
+        return Promise.resolve({
+          type: "language-server",
+          data: "data for language server",
+        });
+      });
+
+      await promise;
+      expect(eventHandler).toHaveBeenCalledWith("data for language server");
+    });
+
+    it("calls the console event handler", async () => {
+      const scriptingService = getScriptingService();
+
+      const { promise, resolve } = lock();
+      const eventHandler = vi.fn(resolve);
+      scriptingService.registerConsoleEventHandler(eventHandler);
+
+      // Let the next data call return an event for the console
+      _jsonDataService.data.mockImplementationOnce(() => {
+        return Promise.resolve({
+          type: "console",
+          data: { text: "my console text", stderr: false },
+        });
+      });
+
+      await promise;
+      expect(eventHandler).toHaveBeenCalledWith({
+        text: "my console text",
+        stderr: false,
+      });
+    });
+  });
+});
