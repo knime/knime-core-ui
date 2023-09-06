e7f574e28113683441bdceae5df0dee5e2663d28
Carsten Haubold <carsten.haubold@knime.com>
2023-09-06 13:58:48 +0200

    Export EditorService
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [631583b6cfeeb632928e3e26c2423d28d019fea4](https://github.com/knime/knime-scripting-editor/commit/631583b6cfeeb632928e3e26c2423d28d019fea4)

--- org.knime.scripting.editor.js/lib/main.ts
+++ org.knime.scripting.editor.js/lib/main.ts
@@ -1,11 +1,18 @@
 import ScriptingEditor from "../src/components/ScriptingEditor.vue";
 import CodeEditor from "../src/components/CodeEditor.vue";
 import OutputConsole from "../src/components/OutputConsole.vue";
+import { EditorService } from "../src/editor-service";
 import { getScriptingService } from "../src/scripting-service";
 import type {
   NodeSettings,
   ScriptingServiceType,
 } from "../src/scripting-service";
 
-export { ScriptingEditor, CodeEditor, OutputConsole, getScriptingService };
+export {
+  ScriptingEditor,
+  CodeEditor,
+  OutputConsole,
+  getScriptingService,
+  EditorService,
+};
 export type { NodeSettings, ScriptingServiceType };
