d67d6355405b9e9f8baf2093711bd2fe81bd76f0
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-01-15 14:25:24 +0100

    AP-20872: Simplify console handler
    It does not need to be a ref
    
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [e7249a9cbb6dffc0ba72c1a8aac946b06e3eefc2](https://github.com/knime/knime-scripting-editor/commit/e7249a9cbb6dffc0ba72c1a8aac946b06e3eefc2)

--- org.knime.scripting.editor.js/src/consoleHandler.ts
+++ org.knime.scripting.editor.js/src/consoleHandler.ts
@@ -1,14 +1,11 @@
 import type { ConsoleHandler } from "@/components/OutputConsole.vue";
-import { shallowRef } from "vue";
 
-export const consoleHandlerStore = shallowRef<ConsoleHandler>({
+export let consoleHandler = {
   writeln: (text) => consola.error("Console not yet initialized", text),
   write: (text) => consola.error("Console not yet initialized", text),
   clear: () => consola.error("Console not yet initialized"),
-});
-
-export const consoleHandler = {
-  writeln: (text) => consoleHandlerStore.value.writeln(text),
-  write: (text) => consoleHandlerStore.value.write(text),
-  clear: () => consoleHandlerStore.value.clear(),
 } as ConsoleHandler;
+
+export const setConsoleHandler = (handler: ConsoleHandler) => {
+  consoleHandler = handler;
+};
