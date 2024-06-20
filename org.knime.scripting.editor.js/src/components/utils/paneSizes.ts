737a3c185faacdbeba710826d0d72d348430ca1c
David Hickey <david.hickey@tngtech.com>
2024-06-20 18:59:34 +0200

    AP-22640: Collapse left pane when window gets too small
    AP-22640 (Polish Spacing & Styling in Script Editor)
    
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [0a85ea43900589173b6f8b6dd6b66c0a85110c72](https://github.com/knime/knime-scripting-editor/commit/0a85ea43900589173b6f8b6dd6b66c0a85110c72)

--- org.knime.scripting.editor.js/src/components/utils/paneSizes.ts
+++ org.knime.scripting.editor.js/src/components/utils/paneSizes.ts
@@ -1,4 +1,5 @@
-export const MIN_WIDTH_FOR_DISPLAYING_PANES = 300;
+export const MIN_WIDTH_FOR_DISPLAYING_PANES = 400;
+export const MIN_WIDTH_FOR_DISPLAYING_LEFT_PANE = 700;
 
 export type PaneSizes = {
   [key in "left" | "right" | "bottom"]: number;
