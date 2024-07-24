e0b381be4fcabf20a277660f67df0e0e9fea2208
Robin Gerling <robin.gerling@knime.com>
2024-07-24 17:06:14 +0200

    UIEXT-2015: Bump ui-extension-service to version 0.36.0 and ui-ext-renderer to 1.0.11
    UIEXT-2015 (Separate image generation and reporting in extension config)
    
    
    (This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: c8245ac7c3d5b2693ac08d5dce8267ea04b21b51)

--- org.knime.scripting.editor.js/src/components/OutputConsole.vue
+++ org.knime.scripting.editor.js/src/components/OutputConsole.vue
@@ -4,7 +4,7 @@ import type { XOR } from "ts-xor";
 import { onMounted, onUnmounted, ref } from "vue";
 import TrashIcon from "@knime/styles/img/icons/trash.svg";
 // @ts-ignore
-import * as knimeColors from "@knime/styles/colors/knimeColors.mjs";
+import * as knimeColors from "@knime/styles/colors/knimeColors.ts";
 import { FunctionButton } from "@knime/components";
 import type { ITerminalInitOnlyOptions, ITerminalOptions, ITheme } from "xterm";
 import { Terminal } from "xterm";
