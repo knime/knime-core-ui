dbaacada3f672bedcb96cb1163c071940c94cee3
David Hickey <david.hickey@tngtech.com>
2024-10-01 11:52:27 +0200

    AP-23173: update and improve tests for scripting editor and its bottom pane
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [1075ea14de2d57c186a42d93455f3a8a87e75582](https://github.com/knime/knime-scripting-editor/commit/1075ea14de2d57c186a42d93455f3a8a87e75582)

--- org.knime.scripting.editor.js/src/components/__tests__/ScriptingEditorBottomPane.test.ts
+++ org.knime.scripting.editor.js/src/components/__tests__/ScriptingEditorBottomPane.test.ts
@@ -2,7 +2,7 @@ import { afterEach, describe, expect, it, vi } from "vitest";
 import { flushPromises, mount } from "@vue/test-utils";
 import { type ConsoleHandler } from "../OutputConsole.vue";
 import ScriptingEditorBottomPane from "../ScriptingEditorBottomPane.vue";
-import { FunctionButton } from "@knime/components";
+import { nextTick } from "vue";
 
 describe("ScriptingEditorBottomPane", () => {
   const { consoleHandler } = vi.hoisted(() => ({
@@ -35,6 +35,7 @@ describe("ScriptingEditorBottomPane", () => {
     const wrapper = mount(ScriptingEditorBottomPane, {
       props: args.props,
       slots: args.slots,
+      attachTo: "body",
     });
     await flushPromises();
 
@@ -45,38 +46,122 @@ describe("ScriptingEditorBottomPane", () => {
     vi.clearAllMocks();
   });
 
-  it("clears console when clear button is clicked", async () => {
-    const { wrapper, consoleHandler } = await doMount();
-
-    // get clear Button and Click it
-    const button = wrapper.findComponent(FunctionButton);
-    button.vm.$emit("click");
-    await flushPromises();
-
-    expect(consoleHandler.clear).toHaveBeenCalled();
-  });
-
-  it("displays content passed by slot", async () => {
+  it("displays content passed by slot (but only selected tab)", async () => {
     const { wrapper } = await doMount({
       props: {
-        slottedTabs: [{ value: "someSlottedThing", label: "SomeSlottedThing" }],
+        slottedTabs: [
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThing",
+            label: "SomeSlottedThing",
+          },
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThing2",
+            label: "SomeSlottedThing2",
+          },
+        ],
       },
       slots: {
-        someSlottedThing: "<div id='test-slotted-id'>Test</div>",
+        "bottomPaneTabSlot:someSlottedThing":
+          "<div id='test-slotted-id'>Test</div>",
+        "bottomPaneTabSlot:someSlottedThing2":
+          "<div id='test-slotted-id-2'>Test</div>",
       },
     });
 
-    expect(wrapper.find("#test-slotted-id").exists()).toBeTruthy();
+    expect(wrapper.find("#test-slotted-id").isVisible()).toBeTruthy();
+    expect(wrapper.find("#test-slotted-id-2").isVisible()).toBeFalsy();
+  });
+
+  it("displays extra controls for each slot (but only for selected tab)", async () => {
+    const { wrapper } = await doMount({
+      props: {
+        slottedTabs: [
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThing",
+            label: "SomeSlottedThing",
+            associatedControlsSlotName:
+              "bottomPaneControlsTabSlot:someSlottedThingControls",
+          },
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThingControls",
+            label: "Controls",
+          },
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThing2",
+            label: "SomeSlottedThing2",
+            associatedControlsSlotName:
+              "bottomPaneControlsTabSlot:someSlottedThing2Controls",
+          },
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThing2Controls",
+            label: "Controls2",
+          },
+        ],
+      },
+      slots: {
+        "bottomPaneTabSlot:someSlottedThing":
+          "<div id='test-slotted-id'>Test</div>",
+        "bottomPaneControlsTabSlot:someSlottedThingControls":
+          "<div id='test-slotted-id-2'>Test</div>",
+        "bottomPaneTabSlot:someSlottedThing2":
+          "<div id='test-slotted-id-3'>Test</div>",
+        "bottomPaneControlsTabSlot:someSlottedThing2Controls":
+          "<div id='test-slotted-id-4'>Test</div>",
+      },
+    });
+
+    expect(wrapper.find("#test-slotted-id").isVisible()).toBeTruthy();
+    expect(wrapper.find("#test-slotted-id-2").isVisible()).toBeTruthy();
+    expect(wrapper.find("#test-slotted-id-3").isVisible()).toBeFalsy();
+    expect(wrapper.find("#test-slotted-id-4").isVisible()).toBeFalsy();
+  });
+
+  it("lets you switch between tabs and their associated controls", async () => {
+    const { wrapper } = await doMount({
+      props: {
+        slottedTabs: [
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThing",
+            associatedControlsSlotName:
+              "bottomPaneControlsTabSlot:someSlottedThingControls",
+            label: "SomeSlottedThing",
+          },
+          {
+            slotName: "bottomPaneTabSlot:someSlottedThing2",
+            label: "SomeSlottedThing2",
+          },
+        ],
+      },
+      slots: {
+        "bottomPaneTabSlot:someSlottedThing":
+          "<div id='test-slotted-id-1a'>Test</div>",
+        "bottomPaneControlsTabSlot:someSlottedThingControls":
+          "<div id='test-slotted-id-1b'>Test</div>",
+        "bottomPaneTabSlot:someSlottedThing2":
+          "<div id='test-slotted-id-2'>Test</div>",
+      },
+    });
+
+    expect(wrapper.find("#test-slotted-id-1a").isVisible()).toBeTruthy();
+    expect(wrapper.find("#test-slotted-id-1b").isVisible()).toBeTruthy();
+    expect(wrapper.find("#test-slotted-id-2").isVisible()).toBeFalsy();
+
+    await wrapper.findAll("label")[1].trigger("click");
+    await nextTick();
+
+    expect(wrapper.find("#test-slotted-id-1a").isVisible()).toBeFalsy();
+    expect(wrapper.find("#test-slotted-id-1b").isVisible()).toBeFalsy();
+    expect(wrapper.find("#test-slotted-id-2").isVisible()).toBeTruthy();
   });
 
   it("displays slotted console status", async () => {
     const { wrapper } = await doMount({
       slots: {
-        "console-status": "<div id='test-id'>Test</div>",
+        "status-label": "<div id='test-id'>Test</div>",
       },
     });
 
     const slotContent = wrapper.find("#test-id");
-    expect(slotContent.exists()).toBeTruthy();
+    expect(slotContent.isVisible()).toBeTruthy();
   });
 });
