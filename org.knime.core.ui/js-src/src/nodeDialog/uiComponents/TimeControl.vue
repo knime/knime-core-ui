ce58fa84371c03f24445d22c5b1619a29199b57e
David Hickey <david.hickey@tngtech.com>
2024-10-23 15:16:07 +0200

    UIEXT-1653: wipwopwep
    UIEXT-1653 (Web UI for Time Modifier (aka Modify Time))

---
+++ org.knime.core.ui/js-src/src/nodeDialog/uiComponents/TimeControl.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { DateTimeInput } from "@knime/components/date-time-input";
+import useDialogControl from "../composables/components/useDialogControl";
+import LabeledControl from "./label/LabeledControl.vue";
+import { rendererProps } from "@jsonforms/vue";
+const props = defineProps(rendererProps());
+const { control, disabled, onChange } = useDialogControl<string>({ props });
+
+const options = computed(() => control.value.uischema.options);
+const minimum = computed(() =>
+  options.value?.minimum ? new Date(options.value.minimum) : null,
+);
+const maximum = computed(() =>
+  options.value?.maximum ? new Date(options.value.maximum) : null,
+);
+</script>
+
+<template>
+  <LabeledControl
+    #default="{ labelForId }"
+    :control="control"
+    @controlling-flow-variable-set="onChange"
+  >
+    <!-- @vue-ignore -->
+    <DateTimeInput
+      :id="labelForId"
+      two-lines
+      :model-value="new Date(control.data)"
+      class="date-time"
+      :required="true"
+      :show-time="true"
+      :show-seconds="options?.showSeconds"
+      :show-milliseconds="props.schema.showAdvancedSettings"
+      :show-date="false"
+      :timezone="options?.timezone"
+      :date-format="options?.dateFormat"
+      :min="minimum"
+      :max="maximum"
+      compact
+      :disabled="disabled"
+      @update:model-value="onChange"
+    />
+  </LabeledControl>
+</template>
