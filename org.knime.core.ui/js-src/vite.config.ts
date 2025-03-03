59b93c65f9aa9b60b1f1752ff598d7750c2c3210
Paul Baernreuther <paul.baernreuther@knime.com>
2025-03-03 10:25:21 +0100

    UIEXT-2598: Fix vite.config, disable sonar in pipeline
    UIEXT-2598 (Fix vite.config in core-ui)

--- org.knime.core.ui/js-src/vite.config.ts
+++ org.knime.core.ui/js-src/vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import type { LibraryOptions } from 'vite';
-import { loadEnv } from 'vite';
 
 /**
  * NOTE: If you add a new library, make sure it is wrapped in a <div> with the
@@ -47,74 +46,70 @@ const getCurrentLibrary = (mode: string) => {
 };
 
 // https://vitest.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-    return {
-        define: {
-            'process.env': env // needed by v-calendar
-        },
-        plugins: [
-            vue(),
-            svgLoader(),
-            cssInjectedByJsPlugin({
-                styleId: 'knime-ui-table',
-                injectCodeFunction: function injectCodeFunction(cssCode) {
-                    try {
-                        if (typeof document !== 'undefined') {
-                            const elementStyle = document.createElement('style');
-                            elementStyle.appendChild(document.createTextNode(cssCode));
-                            document.head.prepend(elementStyle);
-                        }
-                    } catch (e) {
-                        // eslint-disable-next-line no-console
-                        console.error('vite-plugin-css-injected-by-js', e);
+export default defineConfig(({ mode }) => ({
+    define: {
+        'process.env': { NODE_ENV: process.env.NODE_ENV }// needed by v-calendar
+    },
+    plugins: [
+        vue(),
+        svgLoader(),
+        cssInjectedByJsPlugin({
+            styleId: 'knime-ui-table',
+            injectCodeFunction: function injectCodeFunction(cssCode) {
+                try {
+                    if (typeof document !== 'undefined') {
+                        const elementStyle = document.createElement('style');
+                        elementStyle.appendChild(document.createTextNode(cssCode));
+                        document.head.prepend(elementStyle);
                     }
+                } catch (e) {
+                    // eslint-disable-next-line no-console
+                    console.error('vite-plugin-css-injected-by-js', e);
                 }
-            })
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@@': fileURLToPath(new URL('.', import.meta.url))
+        },
+        dedupe: [
+            'vue' // see https://github.com/vuejs/core/issues/4344#issuecomment-899064501
+        ]
+    },
+    build: {
+        lib: getCurrentLibrary(mode),
+        emptyOutDir: false,
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue' // maps to window.Vue which must be provided by the consuming app
+                }
+            }
+        }
+    },
+    test: {
+        include: ['src/**/__tests__/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        environment: 'jsdom',
+        reporters: ['default', 'junit'],
+        deps: { inline: ['consola'] },
+        setupFiles: [
+            fileURLToPath(new URL('test-setup/vitest.setup.js', import.meta.url))
         ],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url)),
-                '@@': fileURLToPath(new URL('.', import.meta.url))
-            },
-            dedupe: [
-                'vue' // see https://github.com/vuejs/core/issues/4344#issuecomment-899064501
-            ]
-        },
-        build: {
-            lib: getCurrentLibrary(mode),
-            emptyOutDir: false,
-            rollupOptions: {
-                external: ['vue'],
-                output: {
-                    globals: {
-                        vue: 'Vue' // maps to window.Vue which must be provided by the consuming app
-                    }
-                }
-            }
-        },
-        test: {
-            include: ['src/**/__tests__/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-            environment: 'jsdom',
-            reporters: ['default', 'junit'],
-            deps: { inline: ['consola'] },
-            setupFiles: [
-                fileURLToPath(new URL('test-setup/vitest.setup.js', import.meta.url))
+        coverage: {
+            all: true,
+            exclude: [
+                'buildtools/', 'coverage/**', 'dist/**', 'webapps-common/**', 'lib/**', '**/*.d.ts',
+                '**/__tests__/**', 'test-setup/**', '**/{vite,vitest,postcss}.config.{js,cjs,mjs,ts}',
+                '**/.{eslint,prettier,stylelint}rc.{js,cjs,yml}', '**/types/**'
             ],
-            coverage: {
-                all: true,
-                exclude: [
-                    'buildtools/', 'coverage/**', 'dist/**', 'webapps-common/**', 'lib/**', '**/*.d.ts',
-                    '**/__tests__/**', 'test-setup/**', '**/{vite,vitest,postcss}.config.{js,cjs,mjs,ts}',
-                    '**/.{eslint,prettier,stylelint}rc.{js,cjs,yml}', '**/types/**'
-                ],
-                reporter: ['html', 'text', 'lcov']
-            },
-            outputFile: {
-                junit: 'test-results/junit.xml' // needed for Bitbucket Pipeline, see https://support.atlassian.com/bitbucket-cloud/docs/test-reporting-in-pipelines/
-            }
+            reporter: ['html', 'text', 'lcov']
         },
-        envPrefix: 'KNIME_'
-    };
-});
+        outputFile: {
+            junit: 'test-results/junit.xml' // needed for Bitbucket Pipeline, see https://support.atlassian.com/bitbucket-cloud/docs/test-reporting-in-pipelines/
+        }
+    },
+    envPrefix: 'KNIME_'
+}));
