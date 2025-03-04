9bd82bd61d11073e7a3eecefd81d887a5790324e
Robin Gerling <robin.gerling@knime.com>
2025-03-04 12:58:53 +0100

    UIEXT-2314: Update dependencies
    UIEXT-2314 (Nominal Row Filter Widget: No value selected upon first re-execution)

--- org.knime.core.ui/js-src/test-setup/vitest.setup.integration.js
+++ org.knime.core.ui/js-src/test-setup/vitest.setup.integration.js
@@ -1,9 +1,9 @@
-require("consola");
-
 import { vi } from "vitest";
 import * as Vue from "vue";
+import consola from "consola";
 
 window.Vue = Vue;
+window.global.consola = consola;
 
 vi.mock("@knime/ui-extension-service");
 vi.mock("@knime/ui-extension-service/internal");
--- org.knime.core.ui/js-src/test-setup/vitest.setup.js
+++ org.knime.core.ui/js-src/test-setup/vitest.setup.js
@@ -1,11 +1,11 @@
-require("consola");
-
 import { vi } from "vitest";
 import * as Vue from "vue";
+import consola from "consola";
 
 window.Vue = Vue;
-
 window.alert = vi.fn();
+
+window.global.consola = consola;
 
 vi.mock("@knime/ui-extension-service");
 vi.mock("@knime/ui-extension-service/internal");
