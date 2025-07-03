42878a78f2b8105e77d265f80930be8bfce64895
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-07-03 13:59:21 +0200

    AP-24607: Update eslint config for eslint 9
    AP-24607 (Update JS dependencies of Scripting Editors)
    
    
    (This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: 5f8cc8757a53a574b3a4e23b2d6f734a4cb013f3)

---
+++ org.knime.scripting.editor.js/eslint.config.js
@@ -0,0 +1,17 @@
+import vitest from "@vitest/eslint-plugin";
+
+import knimeVitest from "@knime/eslint-config/vitest.js";
+import createKnimeVueTSConfig from "@knime/eslint-config/vue3-typescript.js";
+
+export default [
+  ...createKnimeVueTSConfig("./tsconfig.eslint.json"),
+  ...knimeVitest,
+  {
+    plugins: {
+      vitest,
+    },
+    rules: {
+      "vitest/no-import-node-test": "error",
+    },
+  },
+];
