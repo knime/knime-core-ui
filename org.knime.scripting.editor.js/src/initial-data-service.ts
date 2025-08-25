a92ba90cfad2b0ab6ba099a09e21b57709cb6a7c
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-08-25 14:00:19 +0200

    AP-24709: Move InitialDataService and SettingsService to init.ts
    AP-24709 (Do not initialize on import in @knime/scripting-editor but export async initialize function)
    
    
    (This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: b371b7e94241a7fad8f9d67003eca5477aff1d03)

--- org.knime.scripting.editor.js/src/initial-data-service.ts
+++ org.knime.scripting.editor.js/src/initial-data-service.ts
@@ -1,62 +1,25 @@
-import type { InputOutputModel } from "@/components/InputOutputItem.vue";
-import { getSettingsHelper } from "@/settings-helper";
+import {
+  type ConnectionStatus,
+  type GenericInitialData,
+  type InitialDataServiceType,
+  type InputConnectionInfo,
+  type KAIConfig,
+  type PortConfig,
+  type PortConfigs,
+  type PortViewConfig,
+  initialDataService,
+} from "./init";
 
-export type PortViewConfig = {
-  label: string;
-  portViewIdx: number;
+export type {
+  PortViewConfig,
+  PortConfig,
+  PortConfigs,
+  ConnectionStatus,
+  InputConnectionInfo,
+  KAIConfig,
+  GenericInitialData,
+  InitialDataServiceType,
 };
-
-export type PortConfig = {
-  /**
-   * null if no node is connected to an input port
-   */
-  nodeId: string | null;
-  portIdx: number;
-  portViewConfigs: PortViewConfig[];
-  portName: string;
-};
-
-export type PortConfigs = {
-  inputPorts: PortConfig[];
-};
-
-export type ConnectionStatus =
-  /** The input is not connected */
-  | "MISSING_CONNECTION"
-  /** The input is connected, but the predecessor is not configured */
-  | "UNCONFIGURED_CONNECTION"
-  /** The input is connected and configured, but the predecessor is not executed */
-  | "UNEXECUTED_CONNECTION"
-  /** The input is connected, configured, and executed */
-  | "OK";
-
-export type InputConnectionInfo = {
-  status: ConnectionStatus;
-  isOptional: boolean;
-};
-
-export type KAIConfig = {
-  hubId: string;
-  isKaiEnabled: boolean;
-};
-
-export type GenericInitialData = {
-  inputPortConfigs: PortConfigs;
-  inputObjects: InputOutputModel[];
-  flowVariables: InputOutputModel;
-  inputConnectionInfo: InputConnectionInfo[];
-  outputObjects?: InputOutputModel[];
-  kAiConfig: KAIConfig;
-};
-
-const loadDataPromise = getSettingsHelper()
-  .getInitialDataAndSettings()
-  .then((data): GenericInitialData => data.initialData);
-
-const initialDataService = {
-  getInitialData: () => loadDataPromise,
-};
-export type InitialDataServiceType = typeof initialDataService;
 
 export const getInitialDataService = (): InitialDataServiceType =>
   initialDataService;
