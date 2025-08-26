f8b217ad08e9b20aefb066112520c5261af7e303
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-08-26 14:54:58 +0200

    AP-24709: Move getInitialDataService to init.ts
    And the types related to the initial data to initial-data-service.ts
    AP-24709 (Do not initialize on import in @knime/scripting-editor but export async initialize function)
    
    
    (This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: 9eb1643f9c0feaf5437b8bbaa69fb870fdbb9976)

--- org.knime.scripting.editor.js/lib/main.ts
+++ org.knime.scripting.editor.js/lib/main.ts
@@ -30,6 +30,7 @@ import type {
 import editor from "@/editor";
 import {
   type InitMockData,
+  getInitialDataService,
   getScriptingService,
   getSettingsService,
   init,
@@ -41,7 +42,6 @@ import {
   type InputConnectionInfo,
   type KAIConfig,
   type PortConfigs,
-  getInitialDataService,
 } from "@/initial-data-service";
 import {
   type ScriptingServiceType,
--- org.knime.scripting.editor.js/src/components/ai-assistant/AiPopupContent.vue
+++ org.knime.scripting.editor.js/src/components/ai-assistant/AiPopupContent.vue
@@ -16,11 +16,12 @@ import WarningIcon from "@knime/styles/img/icons/circle-warning.svg";
 import SendIcon from "@knime/styles/img/icons/paper-flier.svg";
 
 import InfinityLoadingBar from "@/components/InfinityLoadingBar.vue";
-import { getScriptingService, getSettingsService } from "@/init";
 import {
-  type GenericInitialData,
   getInitialDataService,
-} from "@/initial-data-service";
+  getScriptingService,
+  getSettingsService,
+} from "@/init";
+import { type GenericInitialData } from "@/initial-data-service";
 import { type UsageData } from "@/scripting-service";
 import {
   type Message,
