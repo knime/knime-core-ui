7efa24c6efae3ac27072bf5e309cc13cc98d6258
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-08-26 09:46:18 +0200

    AP-24709: Move ScriptingService implementation back to scripting-service.ts
    Into a class that is only initialized by the `init` fn.
    AP-24709 (Do not initialize on import in @knime/scripting-editor but export async initialize function)
    
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [1397173a990df6075a1573465fd7f26ee85e8e96](https://github.com/knime/knime-scripting-editor/commit/1397173a990df6075a1573465fd7f26ee85e8e96)

--- org.knime.scripting.editor.js/src/scripting-service.ts
+++ org.knime.scripting.editor.js/src/scripting-service.ts
@@ -1,7 +1,138 @@
+import type { JsonDataService } from "@knime/ui-extension-service";
+
 import { consoleHandler } from "./consoleHandler";
-import { type ScriptingServiceType, type UsageData, scriptingService } from "./init";
+import { useMainCodeEditorStore } from "./editor";
+import {
+  type PortConfig,
+  type ScriptingServiceType,
+  type UsageData,
+  scriptingService,
+} from "./init";
+import { MonacoLSPConnection } from "./lsp/connection";
+import { KnimeMessageReader, KnimeMessageWriter } from "./lsp/knime-io";
 
 export type { ScriptingServiceType, UsageData };
+
+type LanguageServerStatus = { status: "RUNNING" | "ERROR"; message?: string };
+
+// TODO AP-19341: use Java-to-JS events
+/**
+ * This class is a singleton that polls for events from the
+ * Java backend and calls event handlers.
+ */
+class EventPoller {
+  private _eventHandlers: { [type: string]: (args: any) => void } = {};
+
+  constructor(private readonly jsonDataService: JsonDataService) {
+    this.startPolling().catch(() => {});
+  }
+
+  private async startPolling() {
+    while (true) {
+      const res = await this.jsonDataService.data<{ type: string; data: any }>({
+        method: "ScriptingService.getEvent",
+      });
+
+      if (res) {
+        if (res.type in this._eventHandlers) {
+          this._eventHandlers[res.type](res.data);
+        } else {
+          throw new Error(
+            `Got unexpected event from Java with type ${res.type}`,
+          );
+        }
+      }
+    }
+  }
+
+  public registerEventHandler(type: string, handler: (args: any) => void) {
+    this._eventHandlers[type] = handler;
+  }
+}
+
+export class ScriptingService {
+  private readonly eventPoller: EventPoller;
+
+  /**
+   * Internal constructor — do not call directly.
+   *
+   * This constructor is intended for internal use only.
+   * Please use the `init` method in `init.ts` to create the singleton instance and
+   * access via `getScriptingService()`. Only one instance should be created via the
+   * designated initialization method.
+   *
+   * @internal
+   * @param jsonDataService The service used for JSON data operations.
+   */
+  constructor(private readonly jsonDataService: JsonDataService) {
+    this.eventPoller = new EventPoller(this.jsonDataService);
+  }
+
+  sendToService(methodName: string, options?: any[] | undefined): Promise<any> {
+    return this.jsonDataService.data({
+      method: `ScriptingService.${methodName}`,
+      options,
+    });
+  }
+
+  registerEventHandler(type: string, handler: (args: any) => void) {
+    this.eventPoller.registerEventHandler(type, handler);
+  }
+
+  // TODO move this logic somewhere else to remove the dependency from the
+  // scripting-service to the main editor state
+  async connectToLanguageServer() {
+    const editorModel = useMainCodeEditorStore().value?.editorModel;
+    if (typeof editorModel === "undefined") {
+      throw Error("Editor model has not yet been initialized");
+    }
+    const status = (await this.sendToService(
+      "connectToLanguageServer",
+    )) as LanguageServerStatus;
+    if (status.status === "RUNNING") {
+      return MonacoLSPConnection.create(
+        editorModel,
+        new KnimeMessageReader(),
+        new KnimeMessageWriter(),
+      );
+    } else {
+      throw Error(status.message ?? "Starting the language server failed");
+    }
+  }
+
+  // Parameters need to be a valid port config, otherwise the call will fail
+  // even when callKnimeUiApi is available
+  async isCallKnimeUiApiAvailable(portToTestFor: PortConfig): Promise<boolean> {
+    const baseService = (this.jsonDataService as any).baseService;
+    if (baseService === null) {
+      return false;
+    }
+
+    return (
+      await baseService.callKnimeUiApi!("PortService.getPortView", {
+        nodeId: portToTestFor.nodeId,
+        portIdx: portToTestFor.portIdx,
+        viewIdx: portToTestFor.portViewConfigs[0]?.portViewIdx,
+      })
+    ).isSome;
+  }
+
+  isKaiEnabled(): Promise<boolean> {
+    return this.sendToService("isKaiEnabled");
+  }
+
+  isLoggedIntoHub(): Promise<boolean> {
+    return this.sendToService("isLoggedIntoHub");
+  }
+
+  getAiDisclaimer(): Promise<string> {
+    return this.sendToService("getAiDisclaimer");
+  }
+
+  getAiUsage(): Promise<UsageData | null> {
+    return this.sendToService("getAiUsage");
+  }
+}
 
 // TODO move this to `init.ts` but move the implementation here
 //   init.ts should handle the application state
