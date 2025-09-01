f8da0049c1761072af5feecc67bdee6d27f02a4b
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-09-01 16:33:45 +0200

    Remove tsconfig from eslint for now
    It makes eslint complain about not included files.
    
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [22d23f9bb16d839aa128bbac0d50cacef73e4da6](https://github.com/knime/knime-scripting-editor/commit/22d23f9bb16d839aa128bbac0d50cacef73e4da6)

--- org.knime.scripting.editor.js/eslint.config.js
+++ org.knime.scripting.editor.js/eslint.config.js
@@ -4,7 +4,7 @@ import knimeVitest from "@knime/eslint-config/vitest.js";
 import createKnimeVueTSConfig from "@knime/eslint-config/vue3-typescript.js";
 
 export default [
-  ...createKnimeVueTSConfig("./tsconfig.eslint.json"),
+  ...createKnimeVueTSConfig(),
   ...knimeVitest,
   {
     plugins: {
