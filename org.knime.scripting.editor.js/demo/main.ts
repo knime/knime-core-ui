e82d1bce30de1a875bda0fcc05c723f184664b17
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-09-02 16:31:43 +0200

    AP-24709: Adapt demo app to new service init
    AP-24709 (Do not initialize on import in @knime/scripting-editor but export async initialize function)
    
    This commit was moved from https://github.com/knime/knime-scripting-editor. Original id: [360d66203c22f235cd60844070d86e6a8d981640](https://github.com/knime/knime-scripting-editor/commit/360d66203c22f235cd60844070d86e6a8d981640)

--- org.knime.scripting.editor.js/demo/main.ts
+++ org.knime.scripting.editor.js/demo/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import { Consola, LogLevels } from "consola";
 
-import "./mock-services";
+import { initMocked } from "../src/init";
+
 import App from "./App.vue";
 
 const setupConsola = () => {
@@ -14,6 +15,14 @@ const setupConsola = () => {
 };
 
 setupConsola();
+
+// Real apps would initialize services here:
+// if (import.meta.env.MODE === "development.browser") {
+//   initMocked((await import("./__mocks__/mock-services")).default)
+// } else {
+//   await init();
+// }
+initMocked((await import("./mock-services")).default);
 
 const app = createApp(App);
 app.mount("#app");
--- org.knime.scripting.editor.js/demo/mock-services.ts
+++ org.knime.scripting.editor.js/demo/mock-services.ts
@@ -1,64 +1,5 @@
-// Mock the UI extension service BEFORE any other imports
-import { DialogService, JsonDataService } from "@knime/ui-extension-service";
-
-// Mock JsonDataService to prevent backend connection attempts
-const mockJsonDataService = {
-  sendRequest(method: string, params: any[] = []) {
-    // eslint-disable-next-line no-console
-    console.log(`Mock JsonDataService.sendRequest: ${method}`, params);
-
-    // Return appropriate responses for different methods
-    switch (method) {
-      case "getConfig":
-        return {};
-      case "getInitialData":
-        return {};
-      default:
-        return {};
-    }
-  },
-
-  async initialData() {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-  },
-
-  registerSettingsHandler() {
-    // eslint-disable-next-line no-console
-    console.log("Mock JsonDataService.registerSettingsHandler");
-    return () => {};
-  },
-
-  registerEventHandler() {
-    // eslint-disable-next-line no-console
-    console.log("Mock JsonDataService.registerEventHandler");
-    return () => {};
-  },
-};
-
-// Override the getInstance method
-JsonDataService.getInstance = () => Promise.resolve(mockJsonDataService as any);
-
-// Mock DialogService for display mode
-const mockDialogService = {
-  getInitialDisplayMode() {
-    return "large" as const;
-  },
-
-  addOnDisplayModeChangeCallback() {
-    // eslint-disable-next-line no-console
-    console.log("Mock DialogService.addOnDisplayModeChangeCallback");
-    return () => {};
-  },
-};
-
-DialogService.getInstance = () => Promise.resolve(mockDialogService as any);
-
-// Now import and setup the scripting editor services
-import { getInitialDataService } from "../src/initial-data-service";
 import { createInitialDataServiceMock } from "../src/initial-data-service-browser-mock";
-import { getScriptingService } from "../src/scripting-service";
 import { createScriptingServiceMock } from "../src/scripting-service-browser-mock";
-import { getSettingsService } from "../src/settings-service";
 import { createSettingsServiceMock } from "../src/settings-service-browser-mock";
 
 import { DEFAULT_INITIAL_DATA, DEFAULT_INITIAL_SETTINGS } from "./mock-data";
@@ -98,6 +39,9 @@ const scriptingService = createScriptingServiceMock({
 const initialDataService = createInitialDataServiceMock(DEFAULT_INITIAL_DATA);
 const settingsService = createSettingsServiceMock(DEFAULT_INITIAL_SETTINGS);
 
-Object.assign(getInitialDataService(), initialDataService);
-Object.assign(getScriptingService(), scriptingService);
-Object.assign(getSettingsService(), settingsService);
+export default {
+  scriptingService,
+  initialDataService,
+  settingsService,
+  displayMode: "large" as const,
+};
