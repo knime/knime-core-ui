60d3eed89386678aeaf28138d9490d1978db5c8f
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-11-05 16:14:16 +0100

    AP-25234: scripting-editor: Remove vitest plugin from scripting-editor eslint-config
    It is already included by the knimeVitest config
    
    AP-25234 (Allow usage of JSONForms dialogs generated from NodeParameters in Scripting Editors)

--- js-src/packages/scripting-editor/eslint.config.js
+++ js-src/packages/scripting-editor/eslint.config.js
@@ -1,19 +1,9 @@
-import vitest from "@vitest/eslint-plugin";
-
 import knimeVitest from "@knime/eslint-config/vitest.js";
 import createKnimeVueTSConfig from "@knime/eslint-config/vue3-typescript.js";
 
 export default [
   ...createKnimeVueTSConfig(),
   ...knimeVitest,
-  {
-    plugins: {
-      vitest,
-    },
-    rules: {
-      "vitest/no-import-node-test": "error",
-    },
-  },
   {
     files: ["demo/**/*"],
     rules: { "no-magic-numbers": "off" },
