70af17d47c1339e6abb020b982dad80608e868a8
Tim Crundall <tim.crundall@tngtech.com>
2025-12-02 10:18:12 +0100

    UIEXT-3068: Add abstract file system port provider
    UIEXT-3068 (WebUI-Migration Transfer Files (Table))

---
+++ org.knime.core.ui/src/eclipse/org/knime/core/webui/node/dialog/defaultdialog/internal/file/InputFSPortProvider.java
@@ -0,0 +1,98 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   2 Dec 2025 (Tim Crundall): created
+ */
+package org.knime.core.webui.node.dialog.defaultdialog.internal.file;
+
+import java.util.Optional;
+
+import org.knime.core.webui.node.dialog.defaultdialog.jsonforms.renderers.options.FileChooserRendererOptions.ConnectedFSOptions;
+import org.knime.node.parameters.NodeParametersInput;
+import org.knime.node.parameters.updates.StateProvider;
+
+/**
+ *
+ * @author Tim Crundall, TNG Technology Consulting GmbH
+ */
+public abstract class InputFSPortProvider implements StateProvider<Optional<ConnectedFSOptions>>  {
+
+    /**
+     * This is necessary for nodes with multiple optional file system inports where the default
+     * behavior (which is to take the first available file system port spec) must be overridden and
+     * instead determine the correct port based on {@link NodeParametersInput}.
+     *
+     * Typically used in tandem with {@link WithFileSystem}
+     *
+     * @return group id that identifies the port. E.g. "Destination File System Connection"
+     */
+    protected abstract String getGroupId();
+
+    private static int getPortIndex(final String groupId, final NodeParametersInput context) {
+        var fsPortIndex = context.getPortsConfiguration().getInputPortLocation().get(groupId);
+        if (fsPortIndex == null || fsPortIndex.length == 0) {
+            return -1;
+        }
+
+        return fsPortIndex[0];
+    }
+
+    @Override
+    public void init(final StateProviderInitializer initializer) {
+        initializer.computeBeforeOpenDialog();
+    }
+
+    @Override
+    public Optional<ConnectedFSOptions> computeState(final NodeParametersInput context) {
+        var inputPortIndex = getPortIndex(getGroupId(), context);
+        if (inputPortIndex == -1) {
+            return Optional.empty();
+        }
+        return Optional.of(inputPortIndex)
+            .flatMap(portIndex -> context.getInPortSpec(portIndex)
+                .map(spec -> ConnectedFSOptions.fromSpec(spec, portIndex)));
+    }
+}
