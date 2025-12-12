1fd5e0e3be8823cbd75691dfa0003f9f82317309
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-12-12 16:17:43 +0100

    AP-25234: Add generic Scripting Editor App
    Replacing the demo app.
    
    AP-25234 (Allow usage of JSONForms dialogs generated from NodeParameters in Scripting Editors)

---
+++ js-src/packages/scripting-editor/app/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ScriptingEditor } from "../lib/main";
+
+import { getAppInitialData } from "./app-initial-data";
+
+const initialData = getAppInitialData();
+</script>
+
+<template>
+  <main>
+    <ScriptingEditor
+      :language="initialData.language"
+      :file-name="initialData.fileName"
+      :initial-pane-sizes="{
+        bottom: 0,
+        left: 0,
+        right: 100,
+      }"
+    />
+  </main>
+</template>
+
+<style>
+@import url("@knime/styles/css");
+</style>
