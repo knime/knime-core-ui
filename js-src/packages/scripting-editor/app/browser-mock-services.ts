7a1487aebbd4e699a1f55581cc6df188c96fc9a3
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-12-16 10:58:12 +0100

    AP-25234: Add settingsInitialData to settings-service browser mock
    Note that this changes the parameters of createSettingsServiceMock and consumers
    now need to provide either settings or settingsInitialData.
    
    AP-25234 (Allow usage of JSONForms dialogs generated from NodeParameters in Scripting Editors)

--- js-src/packages/scripting-editor/app/browser-mock-services.ts
+++ js-src/packages/scripting-editor/app/browser-mock-services.ts
@@ -1,11 +1,87 @@
 import { DEFAULT_INITIAL_DATA } from "../src/initial-data-service-browser-mock";
 import { createScriptingServiceMock } from "../src/scripting-service-browser-mock";
-import {
-  DEFAULT_INITIAL_SETTINGS,
-  createSettingsServiceMock,
-} from "../src/settings-service-browser-mock";
+import type { SettingsInitialData } from "../src/settings-service";
+import { createSettingsServiceMock } from "../src/settings-service-browser-mock";
 
 import { type AppInitialData } from "./app-initial-data";
+
+const settingsInitialData = {
+  data: {
+    model: {
+      script: "A dummy script",
+      supportMultipleStatements: false,
+      separator: ";",
+    },
+  },
+  schema: {
+    type: "object",
+    properties: {
+      model: {
+        type: "object",
+        properties: {
+          script: {
+            type: "string",
+            default: "A dummy script",
+          },
+          separator: {
+            type: "string",
+            title: "Statement Separator",
+            description:
+              "The separator used to split multiple statements. Only relevant if 'Support Multiple Statements' is enabled.",
+            default: ";",
+          },
+          supportMultipleStatements: {
+            type: "boolean",
+            title: "Support Multiple Statements",
+            description:
+              "If enabled, the scripting node will support multiple statements execution.",
+            default: false,
+          },
+        },
+      },
+    },
+  },
+  // eslint-disable-next-line camelcase
+  ui_schema: {
+    elements: [
+      {
+        label: "Multiple SQL Statements",
+        type: "Section",
+        description:
+          "Settings for handling multiple SQL statements in the scripting node.",
+        elements: [
+          {
+            type: "Control",
+            scope: "#/properties/model/properties/supportMultipleStatements",
+            options: {
+              format: "checkbox",
+            },
+          },
+          {
+            type: "Control",
+            scope: "#/properties/model/properties/separator",
+          },
+        ],
+      },
+    ],
+  },
+  flowVariableSettings: {},
+  persist: {
+    type: "object",
+    properties: {
+      model: {
+        type: "object",
+        properties: {
+          script: {
+            configKey: "expression",
+          },
+          supportMultipleStatements: {},
+          separator: {},
+        },
+      },
+    },
+  },
+} as unknown as SettingsInitialData;
 
 const initialData = {
   language: "python",
@@ -13,7 +89,7 @@ const initialData = {
   ...DEFAULT_INITIAL_DATA,
 } satisfies AppInitialData;
 const scriptingService = createScriptingServiceMock({});
-const settingsService = createSettingsServiceMock(DEFAULT_INITIAL_SETTINGS);
+const settingsService = createSettingsServiceMock({ settingsInitialData });
 
 export default {
   initialData,
