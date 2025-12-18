c00190d2abac7a0c4176146b74ded34a935562cc
Carsten Haubold <carsten.haubold@knime.com>
2025-12-18 14:45:14 +0100

    AP-25234: Allow auto completion items to be provided by Java for default scripting nodes
    AP-25234 (Allow usage of JSONForms dialogs generated from NodeParameters in Scripting Editors)

--- js-src/packages/scripting-editor/app/app-initial-data.ts
+++ js-src/packages/scripting-editor/app/app-initial-data.ts
@@ -1,8 +1,11 @@
 import { type GenericInitialData, getInitialData } from "../lib/main";
 
+import type { StaticCompletionItem } from "./static-completion-provider";
+
 export type AppInitialData = GenericInitialData & {
   language: string;
   fileName: string;
+  staticCompletionItems?: StaticCompletionItem[];
 };
 
 export const getAppInitialData = () => getInitialData() as AppInitialData;
