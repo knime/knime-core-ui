106f015a76aeb8a7d57d0891aced7ffd8b86c371
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-02-06 15:35:07 +0100

    KDS-546: Refactor DescriptionPopover and FlowVariableButton to use Kds components for improved UI consistency
    KDS-546 (Variable Toggle Button)

--- js-src/packages/core-ui/src/nodeDialog/uiComponents/flowVariables/components/FlowVariableButton.vue
+++ js-src/packages/core-ui/src/nodeDialog/uiComponents/flowVariables/components/FlowVariableButton.vue
@@ -1,51 +1,57 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 
+import { KdsPopover, KdsVariableToggleButton } from "@knime/kds-components";
+
 import { getFlowVariableSettingsProvidedByControl } from "../../../composables/components/useFlowVariables";
-import DialogPopover from "../../../popover/DialogPopover.vue";
 import type { FlowVariableButtonProps } from "../types/FlowVariableButtonProps";
 
-import FlowVariableIcon from "./FlowVariableIcon.vue";
 import FlowVariablePopover from "./FlowVariablePopover.vue";
 
 defineProps<FlowVariableButtonProps>();
 const emit = defineEmits<{
   controllingFlowVariableSet: [string, unknown, string];
 }>();
-const { configPaths } = getFlowVariableSettingsProvidedByControl();
 
-const tooltipPrefix = ref<string | null>(null);
-const setTooltipPrefix = (prefix: string) => {
-  tooltipPrefix.value = prefix;
-};
-const buttonTooltip = "Click to overwrite with or output as flow variable.";
-const tooltip = computed(() => {
-  if (tooltipPrefix.value === null) {
-    return buttonTooltip;
-  }
-  return `${tooltipPrefix.value} ${buttonTooltip}`;
-});
+const { configPaths, flowSettings } =
+  getFlowVariableSettingsProvidedByControl();
+const inSet = computed(() =>
+  Boolean(flowSettings.value?.controllingFlowVariableName),
+);
+const outSet = computed(() =>
+  Boolean(flowSettings.value?.exposedFlowVariableName),
+);
+const err = computed(() =>
+  Boolean(flowSettings.value?.controllingFlowVariableFlawed),
+);
+
+const open = ref(false);
+const activatorEl = ref<HTMLButtonElement | null>(null);
 </script>
 
 <template>
-  <DialogPopover
-    v-if="configPaths.length"
-    popover-width="380px"
-    :tooltip="tooltip"
-  >
-    <template #icon="{ expanded, focused }">
-      <FlowVariableIcon
-        :show="hover || expanded || focused"
-        @tooltip="setTooltipPrefix"
-      />
-    </template>
-    <template #popover>
+  <template v-if="configPaths.length">
+    <KdsVariableToggleButton
+      ref="activatorEl"
+      v-model="open"
+      :in-set="inSet"
+      :out-set="outSet"
+      :error="err"
+      :hidden="!hover && !inSet && !outSet && !err"
+    />
+    <KdsPopover
+      v-model="open"
+      :activator-el="activatorEl"
+      placement="bottom-left"
+      style="width: 380px"
+    >
       <FlowVariablePopover
+        v-if="open"
         @controlling-flow-variable-set="
           (path, value, flowVarName) =>
             emit('controllingFlowVariableSet', path, value, flowVarName)
         "
       />
-    </template>
-  </DialogPopover>
+    </KdsPopover>
+  </template>
 </template>
