f69ccf5b103657199a5cdc3667138e177685c19b
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-02-24 08:50:48 +0100

    KDS-546: KDS-546: Enhance DescriptionPopover to utilize Kds Description component
    KDS-546 (Variable Toggle Button)

--- js-src/packages/core-ui/src/nodeDialog/uiComponents/description/DescriptionPopover.vue
+++ js-src/packages/core-ui/src/nodeDialog/uiComponents/description/DescriptionPopover.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Description } from "@knime/components";
 import { KdsInfoToggleButton } from "@knime/kds-components";
 
 import type { DescriptionPopoverProps } from "./types/DescriptionPopoverProps";
@@ -11,6 +12,25 @@ withDefaults(defineProps<DescriptionPopoverProps>(), {
 
 <template>
   <KdsInfoToggleButton :hidden="!hover">
-    <div v-html="html" />
+    <div class="description-wrapper">
+      <Description class="description" :text="html" render-as-html />
+    </div>
   </KdsInfoToggleButton>
 </template>
+
+<style scoped>
+/** A deep selector is necessary, since Description is a multi-root component
+* (see https://github.com/vuejs/core/issues/5446)
+*/
+.description-wrapper :deep(.description) {
+  max-height: 300px;
+  overflow: auto;
+  pointer-events: auto;
+  font-size: 13px;
+  font-weight: 300;
+  line-height: 18.78px;
+
+  /* Description component line-height-to-font-size-ratio of 26/18 times font size of 13 */
+  color: var(--knime-masala);
+}
+</style>
