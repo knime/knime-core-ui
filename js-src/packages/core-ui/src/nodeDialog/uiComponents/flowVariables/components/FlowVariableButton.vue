1fb90a5649f781580a4129a23ef6af9e2ed9a744
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-02-24 08:37:23 +0100

    KDS-546: fix
    KDS-546 (Variable Toggle Button)

--- js-src/packages/core-ui/src/nodeDialog/uiComponents/flowVariables/components/FlowVariableButton.vue
+++ js-src/packages/core-ui/src/nodeDialog/uiComponents/flowVariables/components/FlowVariableButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
 import { KdsVariableToggleButton } from "@knime/kds-components";
 
@@ -25,17 +25,21 @@ const error = computed(
   () => Boolean(flowSettings.value?.controllingFlowVariableFlawed),
   // what about invalid values of flow variables?
 );
+
+const open = ref(false);
 </script>
 
 <template>
   <KdsVariableToggleButton
     v-if="configPaths.length"
+    v-model="open"
     :in-set="inSet"
     :out-set="outSet"
     :error="error"
     :hidden="!hover && !inSet && !outSet && !error"
   >
     <FlowVariablePopover
+      v-if="open"
       @controlling-flow-variable-set="
         (path, value, flowVarName) =>
           emit('controllingFlowVariableSet', path, value, flowVarName)
